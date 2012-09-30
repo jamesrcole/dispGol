ea7232ff44156e654e98df0087a41a0c1bf12967
James Cole <jamescole@gmail.com>
2012-09-30 21:16:23 +1000

    work in progress stuff

--- causalRelationsDiagrams.js
+++ causalRelationsDiagrams.js
@@ -24,6 +24,10 @@
         $(dispGolDiv).append(canvasHtml);
         this.canvas = $(dispGolDiv).children("canvas").first().get(0);
 
+        
+        // the grid for each moment
+        this.grids = [];
+
 
 
 
@@ -41,77 +45,64 @@
 
         this.drawDiagram = function(stage,universe) {
 
-            // draw the initial universe state
-
             var topGridX = 50;
             topGridY = 50;
 
-            var timeStep = 0;
+            for (var timeStep = 0; timeStep < this.numSteps; timeStep++) {
 
-            this.topGrid = 
-                new Grid(
-                    topGridX,
-                    topGridY,
-                    gridRows,
-                    gridCols,
-                    largeGridCellSize,
-                    smallGridCellSize,
-                    universe,
-                    timeStep
-                )
-            ;
+                this.grids[timeStep] = 
+                    new Grid(
+                        topGridX,
+                        topGridY,
+                        gridRows,
+                        gridCols,
+                        largeGridCellSize,
+                        smallGridCellSize,
+                        universe,
+                        timeStep
+                    )
+                ;
+                this.grids[timeStep].drawGrid();
+                this.grids[timeStep].drawPattern();
+                stage.addChild(this.grids[timeStep].container);
 
-            this.topGrid.drawGrid();
-            this.topGrid.drawPattern();
-            stage.addChild(this.topGrid.container);
+                stage.update();
 
+                var notLastTimestep = timeStep < (this.numSteps - 1);
+                if (notLastTimestep) {
+                    universe.next();
+                    topGridX += 170;
+                }
+
+            }
+
+
+            var selectedAtom = [0,1];
+
+            // hard-coding of selected atom
             var highlight;
-            highlight = this.topGrid.drawCellHighlighted(2,2,false,"yellow");
-            this.topGrid.container.addChild(highlight);
-            
+            highlight = this.grids[0].drawCellHighlighted(selectedAtom[0],selectedAtom[1],false,"yellow");
+            this.grids[0].container.addChild(highlight);
+        
+            // hard-coded highlighting of children
+            var childAtoms = universe.snapshotChildren.getRelatedAtomPositions(0,selectedAtom);
+            for (var i = 0; i < childAtoms.length; i++) {
+                var childAtom = childAtoms[i];
+                highlight = this.grids[1].drawCellHighlighted(childAtom[0],childAtom[1],false,"green");
+                this.grids[1].container.addChild(highlight);
+            };
+
 
             stage.update();
 
-            for (; timeStep < this.numSteps; timeStep++) {
 
-                topGridX += 170;
 
-                var notLastTimestep = timeStep < (this.numSteps - 1);
-
-                if (notLastTimestep) {
-
-                    universe.next();
-
-                    this.topGrid = 
-                        new Grid(
-                            topGridX,
-                            topGridY,
-                            gridRows,
-                            gridCols,
-                            largeGridCellSize,
-                            smallGridCellSize,
-                            universe,
-                            timeStep
-                        )
-                    ;
-
-                    this.topGrid.drawGrid();
-                    this.topGrid.drawPattern();
-                    stage.addChild(this.topGrid.container);
-
-                    stage.update();
-
-                }
-            }
             
-            debugger;
         }
 
 
+
     }
-
-
-
 
 
 
